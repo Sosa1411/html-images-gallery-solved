05745f714d32a565986c1d5702c43a6e89a876ad
Sosa1411 <nicososa2011@live.com>
2025-01-03 21:13:15 -0600

    linked different pages and ran tests

--- tests/main-page-tests.spec.js
+++ tests/main-page-tests.spec.js
@@ -1,17 +1,17 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
-const mainPageUrl = 'http://localhost:5551/';
+import { test, expect } from "@playwright/test";
+const mainPageUrl = "http://localhost:5551/";
 const plantsPageUrl = `${mainPageUrl}pages/plants.html`;
 const modernDesignPageUrl = `${mainPageUrl}pages/modern-design.html`;
 const spacePageUrl = `${mainPageUrl}pages/space.html`;
 const naturePageUrl = `${mainPageUrl}pages/nature.html`;
 
 const preparePath = (path) => {
-  if (path.startsWith('../')) {
+  if (path.startsWith("../")) {
     return preparePath(path.substring(3));
-  } else if (path.startsWith('./')) {
+  } else if (path.startsWith("./")) {
     return preparePath(path.substring(2));
-  } else if (path.startsWith('.') || path.startsWith('/')) {
+  } else if (path.startsWith(".") || path.startsWith("/")) {
     return preparePath(path.substring(1));
   } else {
     return path;
@@ -20,88 +20,88 @@ const preparePath = (path) => {
 
 // Open Main Page before each test and test the page to run
 
-test.beforeEach('Run the Main Page', async ({ page }) => {
+test.beforeEach("Run the Main Page", async ({ page }) => {
   await page.goto(mainPageUrl);
 });
 
-test('The project development server is running', async ({ page }) => {
+test("The project development server is running", async ({ page }) => {
   expect(page.url()).toBe(mainPageUrl);
 });
 
-test('Main Page: Page Heading exists', async ({ page }) => {
+test("Main Page: Page Heading exists", async ({ page }) => {
   await expect(
-    page.getByRole('heading', { name: 'Digital Art Gallery' })
+    page.getByRole("heading", { name: "Digital Art Gallery" })
   ).toBeAttached();
 });
 
 // CSS connection tests
 
-test('Main Page: layout.css should be connected', async ({ page }) => {
+test("Main Page: layout.css should be connected", async ({ page }) => {
   const cssLocator = page.locator('[rel="stylesheet"]');
   const firstCSSLocator = await cssLocator.nth(0);
-  const cssPath = (await firstCSSLocator.getAttribute('href')) || '';
+  const cssPath = (await firstCSSLocator.getAttribute("href")) || "";
   const url = `${mainPageUrl}${preparePath(cssPath)}`;
   const res = await page.request.get(url);
   expect(cssPath.length).toBeGreaterThan(0);
-  expect(cssPath.includes('layout.css')).toBe(true);
+  expect(cssPath.includes("layout.css")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Main Page: image-list.css should be connected', async ({ page }) => {
+test("Main Page: image-list.css should be connected", async ({ page }) => {
   const cssLocator = page.locator('[rel="stylesheet"]');
   const firstCSSLocator = await cssLocator.nth(1);
-  const cssPath = (await firstCSSLocator.getAttribute('href')) || '';
+  const cssPath = (await firstCSSLocator.getAttribute("href")) || "";
   const url = `${mainPageUrl}${preparePath(cssPath)}`;
   const res = await page.request.get(url);
   expect(cssPath.length).toBeGreaterThan(0);
-  expect(cssPath.includes('images-list.css')).toBe(true);
+  expect(cssPath.includes("images-list.css")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Main Page: The header container is a flex container, which comes from layout.css', async ({
+test("Main Page: The header container is a flex container, which comes from layout.css", async ({
   page,
 }) => {
-  const headerLocator = page.locator('header');
-  await expect(headerLocator).toHaveCSS('display', 'flex');
+  const headerLocator = page.locator("header");
+  await expect(headerLocator).toHaveCSS("display", "flex");
 });
 
 test("Main Page: The 'gallery-items' container is a flex container, which comes from image-list.css", async ({
   page,
 }) => {
-  const galleryContainerLocator = page.locator('.gallery-items');
-  await expect(galleryContainerLocator).toHaveCSS('display', 'flex');
+  const galleryContainerLocator = page.locator(".gallery-items");
+  await expect(galleryContainerLocator).toHaveCSS("display", "flex");
 });
 
 // JavaScript connection tests
 
-test('Main Page: JavaScript file relative path points to an existing JS files', async ({
+test("Main Page: JavaScript file relative path points to an existing JS files", async ({
   page,
 }) => {
-  const scriptLocator = page.locator('[defer]');
-  const scriptPath = (await scriptLocator.getAttribute('src')) || '';
+  const scriptLocator = page.locator("[defer]");
+  const scriptPath = (await scriptLocator.getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(scriptPath)}`;
   const res = await page.request.get(url);
   expect(scriptPath.length).toBeGreaterThan(0);
-  expect(scriptPath.includes('scripts.js')).toBe(true);
+  expect(scriptPath.includes("scripts.js")).toBe(true);
   expect.soft(res.status()).toBe(200);
 });
 
-test('Main Page: there should be a single logged message in the console', async ({
+test("Main Page: there should be a single logged message in the console", async ({
   page,
 }) => {
   const logs = [];
-  page.on('console', (message) => {
+  page.on("console", (message) => {
     logs.push(message.text());
   });
   await page.goto(mainPageUrl);
-  expect(logs.includes('The scripts file is connected!')).toBeTruthy();
+  expect(logs.includes("The scripts file is connected!")).toBeTruthy();
 });
 
-test('Main Page: there should be no error messages in the console', async ({
+test("Main Page: there should be no error messages in the console", async ({
   page,
 }) => {
   const errors = [];
-  page.on('pageerror', (exception) => {
+  page.on("pageerror", (exception) => {
     errors.push(exception);
   });
   await page.goto(mainPageUrl);
@@ -110,126 +110,126 @@ test('Main Page: there should be no error messages in the console', async ({
 
 // Images loading tests
 
-test('Main Page: The design-image.jpg image file path is used for the Modern Design card', async ({
+test("Main Page: The design-image.jpg image file path is used for the Modern Design card", async ({
   page,
 }) => {
-  await page.waitForLoadState('domcontentloaded');
-  const images = page.locator('img');
-  const imageSrc = (await images.nth(0).getAttribute('src')) || '';
+  await page.waitForLoadState("domcontentloaded");
+  const images = page.locator("img");
+  const imageSrc = (await images.nth(0).getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(imageSrc)}`;
   console.log(url);
   const res = await page.request.get(url);
   expect(imageSrc.length).toBeGreaterThan(0);
-  expect(imageSrc.includes('design-image.jpg')).toBe(true);
+  expect(imageSrc.includes("design-image.jpg")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Main Page: The plant-image.jpg image file path is used for the Plants card', async ({
+test("Main Page: The plant-image.jpg image file path is used for the Plants card", async ({
   page,
 }) => {
-  await page.waitForLoadState('domcontentloaded');
-  const images = page.locator('img');
-  const imageSrc = (await images.nth(1).getAttribute('src')) || '';
+  await page.waitForLoadState("domcontentloaded");
+  const images = page.locator("img");
+  const imageSrc = (await images.nth(1).getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(imageSrc)}`;
   console.log(url);
   const res = await page.request.get(url);
   expect(imageSrc.length).toBeGreaterThan(0);
-  expect(imageSrc.includes('plant-image.jpg')).toBe(true);
+  expect(imageSrc.includes("plant-image.jpg")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Main Page: The space-image.jpg image file path is used for the Space card', async ({
+test("Main Page: The space-image.jpg image file path is used for the Space card", async ({
   page,
 }) => {
-  await page.waitForLoadState('domcontentloaded');
-  const images = page.locator('img');
-  const imageSrc = (await images.nth(2).getAttribute('src')) || '';
+  await page.waitForLoadState("domcontentloaded");
+  const images = page.locator("img");
+  const imageSrc = (await images.nth(2).getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(imageSrc)}`;
   console.log(url);
   const res = await page.request.get(url);
   expect(res.status()).toBe(200);
-  expect(imageSrc.includes('space-image.jpg')).toBe(true);
+  expect(imageSrc.includes("space-image.jpg")).toBe(true);
   expect(imageSrc.length).toBeGreaterThan(0);
 });
 
-test('Main Page: The nature-image.jpg image file path is used for the Nature card', async ({
+test("Main Page: The nature-image.jpg image file path is used for the Nature card", async ({
   page,
 }) => {
-  await page.waitForLoadState('domcontentloaded');
-  const images = page.locator('img');
-  const imageSrc = (await images.nth(3).getAttribute('src')) || '';
+  await page.waitForLoadState("domcontentloaded");
+  const images = page.locator("img");
+  const imageSrc = (await images.nth(3).getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(imageSrc)}`;
   console.log(url);
   const res = await page.request.get(url);
   expect(imageSrc.length).toBeGreaterThan(0);
-  expect(imageSrc.includes('nature-image.jpg')).toBe(true);
+  expect(imageSrc.includes("nature-image.jpg")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
 // Top Navigation tests
 
-test('Main Page: Should open the nature page when clicked in the top navigation', async ({
+test("Main Page: Should open the nature page when clicked in the top navigation", async ({
   page,
 }) => {
   const natureLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Nature' })
+    .locator("nav")
+    .getByRole("link", { name: "Nature" })
     .click();
   expect(page.url()).toBe(naturePageUrl);
 });
 
-test('Main Page: Should open the space page when clicked in the top navigation', async ({
+test("Main Page: Should open the space page when clicked in the top navigation", async ({
   page,
 }) => {
   const spaceLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Space' })
+    .locator("nav")
+    .getByRole("link", { name: "Space" })
     .click();
   expect(page.url()).toBe(spacePageUrl);
 });
 
-test('Main Page: Should open the plants page when clicked in the top navigation', async ({
+test("Main Page: Should open the plants page when clicked in the top navigation", async ({
   page,
 }) => {
   const plantsLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Plants' })
+    .locator("nav")
+    .getByRole("link", { name: "Plants" })
     .click();
   expect(page.url()).toBe(plantsPageUrl);
 });
 
-test('Main Page: Plant details Should open the Modern Design page when clicked in the item card', async ({
+test("Main Page: Plant details Should open the Modern Design page when clicked in the item card", async ({
   page,
 }) => {
   const plantsLink = await page
-    .getByRole('link', { name: 'Plants Details' })
+    .getByRole("link", { name: "Plants Details" })
     .click();
   expect(page.url()).toBe(plantsPageUrl);
 });
 
-test('Main Page: Modern Design details link should open Modern Design page when clicked in the item card', async ({
+test("Main Page: Modern Design details link should open Modern Design page when clicked in the item card", async ({
   page,
 }) => {
   const modernDEsignLink = await page
-    .getByRole('link', { name: 'Modern Design Details' })
+    .getByRole("link", { name: "Modern Design Details" })
     .click();
   expect(page.url()).toBe(modernDesignPageUrl);
 });
 
-test('Main Page: Space details link should open Space page when clicked in the item card', async ({
+test("Main Page: Space details link should open Space page when clicked in the item card", async ({
   page,
 }) => {
   const spaceLink = await page
-    .getByRole('link', { name: 'Space Details' })
+    .getByRole("link", { name: "Space Details" })
     .click();
   expect(page.url()).toBe(spacePageUrl);
 });
 
-test('Main Page: Nature details link should open Nature page when clicked in the item card', async ({
+test("Main Page: Nature details link should open Nature page when clicked in the item card", async ({
   page,
 }) => {
   const natureLink = await page
-    .getByRole('link', { name: 'Nature Details' })
+    .getByRole("link", { name: "Nature Details" })
     .click();
   expect(page.url()).toBe(naturePageUrl);
 });
--- tests/plants-page-tests.spec.js
+++ tests/plants-page-tests.spec.js
@@ -1,22 +1,22 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 // Functions and constants
 
-const mainPageUrl = 'http://localhost:5551/';
+const mainPageUrl = "http://localhost:5551/";
 const mainPageUrlLong = `${mainPageUrl}index.html`;
 const plantsPageUrl = `${mainPageUrl}pages/plants.html`;
 const modernDesignPageUrl = `${mainPageUrl}pages/modern-design.html`;
 const spacePageUrl = `${mainPageUrl}pages/space.html`;
 const naturePageUrl = `${mainPageUrl}pages/nature.html`;
-const morePlantsLink = 'https://unsplash.com/s/photos/plants';
+const morePlantsLink = "https://unsplash.com/s/photos/plants";
 
 const preparePath = (path) => {
-  if (path.startsWith('../')) {
+  if (path.startsWith("../")) {
     return preparePath(path.substring(3));
-  } else if (path.startsWith('./')) {
+  } else if (path.startsWith("./")) {
     return preparePath(path.substring(2));
-  } else if (path.startsWith('.') || path.startsWith('/')) {
+  } else if (path.startsWith(".") || path.startsWith("/")) {
     return preparePath(path.substring(1));
   } else {
     return path;
@@ -25,86 +25,86 @@ const preparePath = (path) => {
 
 // Open Main Page before each test and test the page to run
 
-test.beforeEach('Run the Plants Page', async ({ page }) => {
+test.beforeEach("Run the Plants Page", async ({ page }) => {
   await page.goto(plantsPageUrl);
 });
 
-test('The Plants page is running', async ({ page }) => {
+test("The Plants page is running", async ({ page }) => {
   expect(page.url()).toBe(plantsPageUrl);
 });
 
-test('Plants Page: Page Heading exists', async ({ page }) => {
-  await expect(page.getByRole('heading', { name: 'Plants' })).toBeAttached();
+test("Plants Page: Page Heading exists", async ({ page }) => {
+  await expect(page.getByRole("heading", { name: "Plants" })).toBeAttached();
 });
 
 // CSS connection tests
 
-test('Plants Page: layout.css should be connected', async ({ page }) => {
+test("Plants Page: layout.css should be connected", async ({ page }) => {
   const cssLocator = page.locator('[rel="stylesheet"]');
   const firstCSSLocator = await cssLocator.nth(0);
-  const cssPath = (await firstCSSLocator.getAttribute('href')) || '';
+  const cssPath = (await firstCSSLocator.getAttribute("href")) || "";
   const url = `${mainPageUrl}${preparePath(cssPath)}`;
   const res = await page.request.get(url);
   expect(cssPath.length).toBeGreaterThan(0);
-  expect(cssPath.includes('layout.css')).toBe(true);
+  expect(cssPath.includes("layout.css")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Plants Page: image-detail.css should be connected', async ({ page }) => {
+test("Plants Page: image-detail.css should be connected", async ({ page }) => {
   const cssLocator = page.locator('[rel="stylesheet"]');
   const secondCSSLocator = await cssLocator.nth(1);
-  const cssPath = (await secondCSSLocator.getAttribute('href')) || '';
+  const cssPath = (await secondCSSLocator.getAttribute("href")) || "";
   const url = `${mainPageUrl}${preparePath(cssPath)}`;
   const res = await page.request.get(url);
   expect(cssPath.length).toBeGreaterThan(0);
-  expect(cssPath.includes('image-detail.css')).toBe(true);
+  expect(cssPath.includes("image-detail.css")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
-test('Plants Page: The header container should be a flex container, if layout.css is connected', async ({
+test("Plants Page: The header container should be a flex container, if layout.css is connected", async ({
   page,
 }) => {
-  const headerLocator = page.locator('header');
-  await expect(headerLocator).toHaveCSS('display', 'flex');
+  const headerLocator = page.locator("header");
+  await expect(headerLocator).toHaveCSS("display", "flex");
 });
 
 test("Plants Page: The 'image-details' should a flex container, if the image-list.css is connected", async ({
   page,
 }) => {
-  const imageDetailsContainerLocator = page.locator('.image-details');
-  await expect(imageDetailsContainerLocator).toHaveCSS('display', 'flex');
+  const imageDetailsContainerLocator = page.locator(".image-details");
+  await expect(imageDetailsContainerLocator).toHaveCSS("display", "flex");
 });
 
 // JavaScript connection tests
 
-test('Plants Page: JavaScript file relative path points to an existing JS files', async ({
+test("Plants Page: JavaScript file relative path points to an existing JS files", async ({
   page,
 }) => {
-  const scriptLocator = page.locator('[defer]');
-  const scriptPath = (await scriptLocator.getAttribute('src')) || '';
+  const scriptLocator = page.locator("[defer]");
+  const scriptPath = (await scriptLocator.getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(scriptPath)}`;
   const res = await page.request.get(url);
   expect(scriptPath.length).toBeGreaterThan(0);
-  expect(scriptPath.includes('scripts.js')).toBe(true);
+  expect(scriptPath.includes("scripts.js")).toBe(true);
   expect.soft(res.status()).toBe(200);
 });
 
-test('Plants Page: the scripts.js file should be connected', async ({
+test("Plants Page: the scripts.js file should be connected", async ({
   page,
 }) => {
   const logs = [];
-  page.on('console', (message) => {
+  page.on("console", (message) => {
     logs.push(message.text());
   });
   await page.goto(plantsPageUrl);
-  expect(logs.includes('The scripts file is connected!')).toBeTruthy();
+  expect(logs.includes("The scripts file is connected!")).toBeTruthy();
 });
 
-test('Plants Page: there should be no error messages in the console', async ({
+test("Plants Page: there should be no error messages in the console", async ({
   page,
 }) => {
   const errors = [];
-  page.on('pageerror', (exception) => {
+  page.on("pageerror", (exception) => {
     errors.push(exception);
   });
   await page.goto(plantsPageUrl);
@@ -113,68 +113,68 @@ test('Plants Page: there should be no error messages in the console', async ({
 
 // Images connection tests
 
-test('Plants Page: The plant-image.jpg image file path is used for the main image', async ({
+test("Plants Page: The plant-image.jpg image file path is used for the main image", async ({
   page,
 }) => {
-  await page.waitForLoadState('domcontentloaded');
-  const image = page.locator('img');
-  const imageSrc = (await image.getAttribute('src')) || '';
+  await page.waitForLoadState("domcontentloaded");
+  const image = page.locator("img");
+  const imageSrc = (await image.getAttribute("src")) || "";
   const url = `${mainPageUrl}${preparePath(imageSrc)}`;
   const res = await page.request.get(url);
   expect(imageSrc.length).toBeGreaterThan(0);
-  expect(imageSrc.includes('plant-image.jpg')).toBe(true);
+  expect(imageSrc.includes("plant-image.jpg")).toBe(true);
   expect(res.status()).toBe(200);
 });
 
 // Top Navigation connection tests
 
-test('Plants Page: Should open the nature page when clicked in the top navigation', async ({
+test("Plants Page: Should open the nature page when clicked in the top navigation", async ({
   page,
 }) => {
   const natureLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Nature' })
+    .locator("nav")
+    .getByRole("link", { name: "Nature" })
     .click();
   expect(page.url()).toBe(naturePageUrl);
 });
 
-test('Plants Page: Should open the space page when clicked in the top navigation', async ({
+test("Plants Page: Should open the space page when clicked in the top navigation", async ({
   page,
 }) => {
   const spaceLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Space' })
+    .locator("nav")
+    .getByRole("link", { name: "Space" })
     .click();
   expect(page.url()).toBe(spacePageUrl);
 });
 
-test('Plants Page: Should open the plants page when clicked in the top navigation', async ({
+test("Plants Page: Should open the plants page when clicked in the top navigation", async ({
   page,
 }) => {
   const plantsLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Plants' })
+    .locator("nav")
+    .getByRole("link", { name: "Plants" })
     .click();
   expect(page.url()).toBe(plantsPageUrl);
 });
 
-test('Plants Page: Should open the modern design page when clicked in the top navigation', async ({
+test("Plants Page: Should open the modern design page when clicked in the top navigation", async ({
   page,
 }) => {
   const modernDesignLink = await page
-    .locator('nav')
-    .getByRole('link', { name: 'Modern Design' })
+    .locator("nav")
+    .getByRole("link", { name: "Modern Design" })
     .click();
   expect(page.url()).toBe(modernDesignPageUrl);
 });
 
 // Back to home page link tests
 
-test('Back to home page link should redirect to the main page', async ({
+test("Back to home page link should redirect to the main page", async ({
   page,
 }) => {
   const backHomeLinkLocator = await page
-    .getByRole('link', { name: 'Back to Home Page' })
+    .getByRole("link", { name: "Back to Home Page" })
     .click();
   expect([mainPageUrl, mainPageUrlLong].includes(page.url())).toBeTruthy();
 });
@@ -185,7 +185,7 @@ test('"Prev: Modern Design" link should redirect to the Modern Design page', asy
   page,
 }) => {
   const prevModernDesignLink = await page
-    .getByRole('link', { name: 'Prev: Modern Design' })
+    .getByRole("link", { name: "Prev: Modern Design" })
     .click();
   expect(page.url()).toBe(modernDesignPageUrl);
 });
@@ -194,7 +194,7 @@ test('"Next: Nature" link should redirect to the Nature page', async ({
   page,
 }) => {
   const nexNatureLink = await page
-    .getByRole('link', { name: 'Next: Nature' })
+    .getByRole("link", { name: "Next: Nature" })
     .click();
   expect(page.url()).toBe(naturePageUrl);
 });
@@ -203,7 +203,7 @@ test('"More Modern Design Images" link should use an absolute path to unsplash.c
   page,
 }) => {
   const nexModernDesignLink = await page
-    .getByRole('link', { name: 'More Plants Images' })
+    .getByRole("link", { name: "More Plants Images" })
     .click();
   expect(page.url() === morePlantsLink).toBeTruthy();
 });
